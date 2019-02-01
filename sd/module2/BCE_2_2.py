f4ae50ae8467ba87ef26f1caa6376aa7406e7015
Haibo Yan <Haibo Yan>
2019-02-01 10:20:15 -0800

    First assignment

---
+++ sd/module2/BCE_2_2.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+import re
+
+
+def exchange(currency_value, rate):
+    """Return corresponding currency value by given currency number and exchange rate"""
+    return currency_value * rate
+
+
+# How much us dollars worth as same as 1,000,000 Venezuelan Bolivars
+bolivars_value = 1000000
+us_dollar_value_per_bolivar = 0.016
+us_dollars_value = exchange(bolivars_value, us_dollar_value_per_bolivar)
+print(f"{bolivars_value} Venezuelan Bolivars has same value as {us_dollars_value:.2f} us dollars.")
+
+# How many Bolivars you would need to have today to live for the next 3 years if you require $12,060 (US) per year
+living_standard = 12060
+period = 3
+bolivars_required = exchange(living_standard * period * us_dollars_value, us_dollar_value_per_bolivar)
+print(f"You need {bolivars_required:2f} Venezuelan Bolivars for three years when you live with {living_standard} us dollars a year.")
+
+# how many minutes there will be in the next 3 years, if there are 365.2425 days in a year. Calculate how many Bolivars
+# you would spend per minute, on average
+days_per_year = 365.2425
+minutes_pre_day = 24 * 60
+years = 3
+total_minutes = years * days_per_year * minutes_pre_day
+print(f"{years} years has {total_minutes:.0f} minutes.")
+
+bolivars_per_minute = bolivars_required / total_minutes
+print(f"You need {bolivars_per_minute:.2f} Venezuelan bolivars to survive for every minute.")
+
+# Create the variable, bloody_vikings, containing the value ‘Wonderful Spam! Glorious Spam!’.
+bloody_vikings = 'Wonderful Spam! Glorious Spam!'
+
+# Create a variable, viking_exclamations, containing a two-item list of the values of the two exclamations in bloody_
+# vikings.
+exclamation_expr = r'[^!]*!'
+viking_exclamations = [x.strip() for x in re.findall(exclamation_expr, bloody_vikings)]
+print(viking_exclamations)
+
+# Create a variable, viking_exaggeration, containing the second value in the list assigned to viking_exclamations.
+viking_exaggeration = viking_exclamations[1]
+print(viking_exaggeration)
+
+# Create a variable, upper_exaggeration, containing the completely capitalized (not just the first letter) version of
+# the contents of viking_exaggeration.
+upper_exaggeration = viking_exaggeration.upper()
+print(upper_exaggeration)
+
+# Create a variable, partial_exaggeration, containing the 4th through 8th characters of the string assigned to
+# viking_exaggeration.
+partial_exaggeration = upper_exaggeration[3:8]
+print(partial_exaggeration)
---
+++ sd/module2/README.md
@@ -0,0 +1,19 @@
+# Team Exercise: Python coding (5 points)
+
+Code: [BCE_2_2.py](./BCE_2_2.py)
+
+1. Calculate (using variables, rather than numeric constants) how many US dollars one currently has if one has 1,000,000 Venezuelan Bolivars. Assume that 1 Bolivar is worth $0.016 (US) dollars. Include the Python statements to initialize your variables to the appropriate values, as well as to hold the result of the calculation and print this result out in a readable way.
+ 
+2. Calculate how many Bolivars you would need to have today to live for the next 3 years if you require $12,060 (US) per year, and you keep the sum in cash under your mattress (instead of putting it in a bank to earn interest). Print this result out in a readable way.
+ 
+3. Calculate how many minutes there will be in the next 3 years, if there are 365.2425 days in a year. Calculate how many Bolivars you would spend per minute, on average, based on the calculations here and in task 2 above. Do this using both floating point division and integer division. Print these calculated results out in readable ways.
+ 
+4. Create the variable, bloody_vikings, containing the value ‘Wonderful Spam! Glorious Spam!’.
+ 
+5. Create a variable, viking_exclamations, containing a two-item list of the values of the two exclamations in bloody_vikings.
+ 
+6. Create a variable, viking_exaggeration, containing the second value in the list assigned to viking_exclamations.
+ 
+7. Create a variable, upper_exaggeration, containing the completely capitalized (not just the first letter) version of the contents of viking_exaggeration.
+ 
+8. Create a variable, partial_exaggeration, containing the 4th through 8th characters of the string assigned to viking_exaggeration.
