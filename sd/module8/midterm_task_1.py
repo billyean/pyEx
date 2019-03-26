bd5a79e1527a1883477ba9c6d0153076cc26f696
Haibo Yan <tristan.yim@gmail.com>
2019-03-25 23:40:36 -0700

    Mid Term Coding 1

---
+++ sd/module8/midterm_task_1.py
@@ -0,0 +1,36 @@
+import random
+import time
+import os
+
+
+def random_char():
+    # random pick a character between 'a' to 'z'
+    # chr function will return a character which ascii number is given integer.
+    p = random.randrange(26)
+    return chr(97 + p)
+
+
+def main():
+    select = random_char()
+    max_tries = 9
+    tries = 0
+    while tries < max_tries:
+        guess = input('Please guess what the letter is : ')
+        if guess == 'quit':
+            print('OK, bye!')
+            break
+        if guess == select:
+            print(f'You guessed it in only {tries + 1} tries!’ ')
+            break
+
+        tries += 1
+        print(f"That’s not it, try again!  You have {max_tries - tries} guesses left.")
+        time.sleep(1)
+        os.system('clear')
+
+    if tries == max_tries:
+        print(f"Sorry, you didn’t guess it in {max_tries} tries.  The letter was {select}.")
+
+
+if __name__ == "__main__":
+    main()
