d26ef7eea106ccadf25f0cf862e6a6aecd712354
Haibo Yan <tristan.yim@gmail.com>
2019-03-26 23:45:09 -0700

    Update midterm answer

--- sd/module8/midterm_task_1.py
+++ sd/module8/midterm_task_1.py
@@ -14,22 +14,24 @@ def main():
     select = random_char()
     max_tries = 9
     tries = 0
-    while tries < max_tries:
-        guess = input('Please guess what the letter is : ')
-        if guess == 'quit':
-            print('OK, bye!')
-            break
-        if guess == select:
-            print(f'You guessed it in only {tries + 1} tries!’ ')
+    while True:
+        guess = input("Please guess what the letter is : ")
+        if guess == "quit":
+            print("OK, bye!")
             break
 
         tries += 1
+        if guess == select:
+            print(f"You guessed it in only {tries} tries!")
+            break
+
+        if tries == max_tries:
+            print(f"Sorry, you didn’t guess it in {max_tries} tries.  The letter was {select}.")
+            break
+
         print(f"That’s not it, try again!  You have {max_tries - tries} guesses left.")
         time.sleep(1)
         os.system('clear')
-
-    if tries == max_tries:
-        print(f"Sorry, you didn’t guess it in {max_tries} tries.  The letter was {select}.")
 
 
 if __name__ == "__main__":
--- sd/module8/midterm_task_3.py
+++ sd/module8/midterm_task_3.py
@@ -1,5 +1,6 @@
 import os
 import time
+import sys
 from datetime import datetime
 
 
@@ -25,29 +26,39 @@ def main():
         if select == '000':
             time.sleep(3)
             os.system("clear")
+            sys.exit()
             break
 
         # Displays the in-date and out-date for the ID # accepted in part e, above (with appropriate labels provided).
         # If the ID # is not in the dictionary, displays ‘ID # not found’.
         if select not in id2_in_out_date:
-            print("ID # not found")
+            print(f"ID {select} not found")
             time.sleep(1)
             os.system("clear")
         else:
             print(f"ID : {select}; In Date: {id2_in_out_date[select][0]}; Out Date: {id2_in_out_date[select][1]}")
             time.sleep(2)
-            os.system("clear")
+            break
+    print()
 
     # Displays the ID # (or ID #’s) for the longest period from in-date to out-date, inclusive, as well as the
     # length of the period (with appropriate labels provided).
-    print(id2_in_out_date)
-    max_period_id = max(id2_in_out_date, key=lambda id: datetime.strptime(id2_in_out_date[id][1], '%Y/%m/%d')
-                                                          - datetime.strptime(id2_in_out_date[id][0], '%Y/%m/%d'))
-    max_period_in_date = id2_in_out_date[max_period_id][0]
-    max_period_out_date = id2_in_out_date[max_period_id][1]
-    max_period = datetime.strptime(max_period_out_date, '%Y/%m/%d') - datetime.strptime(max_period_in_date, '%Y/%m/%d')
+    max_period = 0
+    max_period_ids = []
 
-    print(f"ID: {max_period_id}; In Date: {max_period_in_date}; Out Date: {max_period_out_date} has longest period {max_period.days + 1} days")
+    for idn in id2_in_out_date:
+        (in_date, out_date) = id2_in_out_date[idn]
+        p = (datetime.strptime(out_date, '%Y/%m/%d') - datetime.strptime(in_date, '%Y/%m/%d')).days + 1
+        if p > max_period:
+            max_period = p
+            max_period_ids = [idn]
+        elif p == max_period:
+            max_period_ids.append(idn)
+
+    for max_period_id in max_period_ids:
+        (in_date, out_date) = id2_in_out_date[max_period_id]
+        print(f"ID: {max_period_id}; In Date: {in_date}; Out Date: {out_date} has longest period {max_period} days")
+    print()
 
     # Adds a new entry to the dictionary key = 005, value = [2019/03/24, 2019/04/04]
     id2_in_out_date['005'] = ['2019/03/24', '2019/04/04']
@@ -56,6 +67,7 @@ def main():
     for idn in id2_in_out_date:
         print(f"ID : {idn}; In Date: {id2_in_out_date[idn][0]}; Out Date: {id2_in_out_date[idn][1]}")
 
+    # Writes the values in the dictionary to the file midterm_output.txt.
     with open('midterm_output.txt', 'wt') as outputf:
         for idn in id2_in_out_date:
             print(f"{idn}\t{id2_in_out_date[idn][0]}\t{id2_in_out_date[idn][1]}", file=outputf)
