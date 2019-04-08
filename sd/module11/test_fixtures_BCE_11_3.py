84d48f2e3f30841d27a32804a7cc1cbf739aec05
Haibo Yan <tristan.yim@gmail.com>
2019-04-08 16:42:38 -0700

    Add Ex2 iteration 6

--- sd/module11/test_fixtures_BCE_11_3.py
+++ sd/module11/test_fixtures_BCE_11_3.py
@@ -112,3 +112,42 @@ def test_add_increases_count(db_with_3_tasks):
 
     # THEN the count increases by 1
     assert tasks.count() == 4
+
+
+@pytest.fixture(scope='function')
+def func_scope():
+    """A function scope fixture"""
+
+
+@pytest.fixture(scope='module')
+def mod_scope():
+    """A module scope fixture"""
+
+
+@pytest.fixture(scope='session')
+def sess_scope():
+    """A session scope fixture"""
+
+
+@pytest.fixture(scope='class')
+def class_scope():
+    """A class scope fixture"""
+
+
+def test_1(sess_scope, mod_scope, func_scope):
+    """Test using session, module and function scope fixtures."""
+
+
+def test_2(sess_scope, mod_scope, func_scope):
+    """Demo is more fun with multiple tests."""
+
+
+@pytest.mark.usefixtures('class_scope')
+class TestSomething ():
+    """Demo class scope fixtures"""
+
+    def test_3(self):
+        """Test using a class scope fixture."""
+
+    def test_4(self):
+        """Again , multiple tests are more fun."""
