2b8ec96caa09c281f9fa1877aa995ed27183e7ff
Haibo Yan <tristan.yim@gmail.com>
2019-04-08 16:00:11 -0700

    Add Ex2 iteration 3 and iteration 4

--- sd/module11/test_fixtures_BCE_11_3.py
+++ sd/module11/test_fixtures_BCE_11_3.py
@@ -31,3 +31,28 @@ def test_add_returns_valid_id(task_db):
     new_task = Task('do something')
     task_id = tasks.add(new_task)
     assert isinstance(task_id, int)
+
+
+@pytest.fixture()
+def a_tuple():
+    """Return something more interesting."""
+    return 1, 'foo', None, {'bar': 23}
+
+
+def test_a_tuple(a_tuple):
+    """Demo the a_tuple fixture."""
+    assert a_tuple[3]['bar'] == 32
+
+
+@pytest.fixture()
+def some_other_data():
+    """Raise an exception from fixture."""
+    x = 43
+    assert x == 42
+
+
+def test_other_data(some_other_data):
+    """Raise an exception from fixture."""
+    pass
+
+
