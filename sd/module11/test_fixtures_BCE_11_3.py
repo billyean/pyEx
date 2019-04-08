abaf7df440e22c23c41f1ad50fbda8b6b8dbfe2c
Haibo Yan <tristan.yim@gmail.com>
2019-04-08 16:27:13 -0700

    Add Ex2 iteration 5

--- sd/module11/test_fixtures_BCE_11_3.py
+++ sd/module11/test_fixtures_BCE_11_3.py
@@ -56,3 +56,59 @@ def test_other_data(some_other_data):
     pass
 
 
+# Reminder of Task constructor interface
+# Task(summary= None, owner= None, done= False, id= None)
+# summary is required
+# owner and done are optional
+# id is set by database
+
+@pytest.fixture()
+def tasks_just_a_few():
+    """All summaries and owners are unique."""
+    return (
+        Task('Write some code', 'Brian', True),
+        Task("Code review Brian's code", 'Katie', False),
+        Task('Fix what Brian did', 'Michelle', False)
+    )
+
+
+@pytest.fixture()
+def tasks_mult_per_owner():
+    """Several owners with several tasks each."""
+    return (
+        Task('Make a cookie', 'Raphael'),
+        Task('Use an emoji', 'Raphael'),
+        Task('Move to Berlin', 'Raphael'),
+
+        Task('Create', 'Michelle'),
+        Task('Inspire', 'Michelle'),
+        Task('Encourage', 'Michelle'),
+
+        Task('Do a handstand', 'Daniel'),
+        Task('Write some books', 'Daniel'),
+        Task('Eat ice cream', 'Daniel')
+    )
+
+
+@pytest.fixture()
+def db_with_3_tasks(task_db, tasks_just_a_few):
+    """Connect db with 3 tasks, all unique"""
+    for task in tasks_just_a_few:
+        tasks.add(task)
+
+
+@pytest.fixture()
+def db_with_mult_per_owner(task_db, tasks_mult_per_owner):
+    """Connect db with 3 tasks, all unique"""
+    for task in tasks_mult_per_owner:
+        tasks.add(task)
+
+
+def test_add_increases_count(db_with_3_tasks):
+    """Test tasks.add() affect on tasks.count()"""
+    # GIVEN a db with 3 tasks
+    # WHEN another task is added
+    tasks.add(Task('throw a party'))
+
+    # THEN the count increases by 1
+    assert tasks.count() == 4
